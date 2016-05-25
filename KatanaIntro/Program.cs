7f89bc005c37eca6febc3fa8359d9215f2ab8e84
Neil Pullinger <ncpullinger+github@gmail.com>
2016-05-25 19:37:22 +0100

    Add project files.

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+
+namespace KatanaIntro
+{
+    /// <summary>
+    /// 1. Install-Package Microsoft.Owin.Hosting
+    /// 2. Install-Package Microsoft.Owin.Host.HttpListener
+    /// To get pretty welcome page:
+    /// 3. Install-Package Microsoft.Owin.Diagnostics and then app.UseWelcomePage()
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Server started...");
+                Console.ReadKey();
+                Console.WriteLine("Server stopped.");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+
+            //app.Run(ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("<h1>Hello World!</h1><p>This is Katana.</p>");
+            //});
+        }
+    }
+}
